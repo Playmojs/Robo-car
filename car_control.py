54f23a0313c7e3f1144b82feffecdc1512d3a72d
Joresund <johanstore2002@gmail.com>
2023-06-19 15:27:46 +0200

    fix: attemped fixing buffer error

--- car_control.py
+++ car_control.py
@@ -5,42 +5,40 @@ import time
 ser = serial.Serial("COM4", baudrate = 115200)
 active_input = 1000
 buffer = 0
-driving_keys = ['w', 'a','s','d']
+driving_keys = ['w','a','s','d']
 stopped = True
 
 print ("press 'escape' to quit, drive with WASD")
 
-while 1:
-    input = ord(msvcrt.getch().decode()) if msvcrt.kbhit() else 1000
-
-    if input == 27:
-        break
-    if chr(input) in driving_keys and input != active_input:
-        active_input = input
-        ser.write(active_input)
-        stopped = False
-        buffer = 0
-        print("Change direction")
-    elif chr(input) not in driving_keys and not stopped:
-        buffer += 1
-        if buffer > 17:
-            active_input = input
-            ser.write(active_input)
-            stopped = True
-            print("Stopping")
-        print("Buffer: ", buffer)
-        
-    time.sleep(1/50)
-
+while (input:=ord(msvcrt.getch().decode()) if msvcrt.kbhit() else 1000) != 27:
+    #Stop if new input is 27 (ascii code for esc)
     #Check if WASD pressed
-        #Compare input with active input
+    if chr(input) in driving_keys:
+        #Compare new input with active input
+        if input!=active_input:
+            #Change active input into new input
+            active_input=input
             #Write new input to serial
-            #Change active input
+            ser.write(active_input)
+            # Set stopped to False
+            stopped=False
+            # Reset buffer
+            buffer=0
+            print("Change direction")
+        print("Driving")
     #If WASD is not pressed
-        #Check if active input is WASD
-            #Check buffer size
-                #Write new input to serial
-            #Else:
-                #Increase buffer
-    #Sleep
+    elif not stopped:
+        # Increment buffer
+        buffer+=1
+        # Check if buffer size >= 10
+        if buffer>=10:
+            # Update active input
+            active_input=input
+            # Write to serial
+            ser.write(active_input)
+            # Set stopped to True
+            print("Stopping")
+        print(f"Buffer: {buffer}")
+    # Wait 0.02 seconds
+    time.sleep(0.02)
 
